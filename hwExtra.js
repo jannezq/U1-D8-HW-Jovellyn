fdafc935535009ca05ca7cd3b5870fbb70718803
Jovellyn Quiapos <jovellynanne11@gmail.com>
2022-11-30 21:44:18 +0000

    worked up to extra question 7

--- hwExtra.js
+++ hwExtra.js
@@ -48,19 +48,39 @@ console.log("Even numbers: ", even);
  Write a piece of code for deleting only even entries from an array.
 */
 
+let length = maxNumArray.length;
+
+for (let i = 0; i < length; i++) {
+    for(let j = 0; j < maxNumArray.length; j++){
+        if (maxNumArray[j] % 2 !== 1 ) {
+            maxNumArray.splice(j,1)
+            
+        }
+    } 
+};
+console.log("Odd numbers only: ", maxNumArray);
 
 
 /* EXTRA 6
  Write a piece of code for removing all the vowels from a string.
 */
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+let noVowels = alphabet.replace(/[aeiou]/gi, "");
 
-/* WRITE YOUR ANSWER HERE */
+console.log("no vowels: ",noVowels);
+
 
 /* EXTRA 7
  Write a piece of code for increasing all the numerical values in a array by 1.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let numArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+
+for (let i = 0; i < numArray.length; i++) {
+    numArray[i] = numArray[i] + 1;
+}
+
+console.log("numArray added by 1 each: ", numArray);
 
 /* EXTRA 8 
  Replace all the strings contained in an array with their length.
--- trial.js
+++ trial.js
@@ -1,16 +1,36 @@
 /* EXTRA 5
  Write a piece of code for deleting only even entries from an array.
 */
+let maxNumArray = [21, 33, 10, 2, 87, 19, 22, 46, 31, 104, 43, 67, 94, 88, 124,906]
+let length = maxNumArray.length;
 
-
-let maxNumArray = [21, 33, 10, 2, 87, 19, 22, 46, 31, 104]
-let even = [];
-
-for (let i = 0; i < maxNumArray.length; i++) {
-    if((maxNumArray[i] % 2) == 0){
-        even.push(maxNumArray[i]);
-     
-    }   
-
+for (let i = 0; i < length; i++) {
+    for(let j = 0; j < maxNumArray.length; j++){
+        if (maxNumArray[j] % 2 !== 1 ) {
+            maxNumArray.splice(j,1)
+            
+        }
+    } 
 };
-console.log("Even numbers: ", even);
+console.log("Odd numbers only: ", maxNumArray);
+
+/* EXTRA 6
+ Write a piece of code for removing all the vowels from a string.
+*/
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+let noVowels = alphabet.replace(/[aeiou]/gi, "");
+
+console.log("no vowels: ",noVowels);
+
+/* EXTRA 7
+ Write a piece of code for increasing all the numerical values in a array by 1.
+*/
+
+let numArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+
+for (let i = 0; i < numArray.length; i++) {
+    numArray[i] = numArray[i] + 1;
+}
+
+console.log("numArray added by 1 each: ", numArray);
