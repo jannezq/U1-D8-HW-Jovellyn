392d79f2277684dc77dc3969f25fa427f9b36521
Jovellyn Quiapos <jovellynanne11@gmail.com>
2022-11-30 15:02:34 +0000

    question 8 done

--- homework.js
+++ homework.js
@@ -160,7 +160,37 @@ console.log("Total cost after discount:", totalCost );
  Then clone it 5 times, and change the licensePlate for each cloned car without affecting the original one.
 */
 
+console.log("\n>------------------Excercise 8---------------------------<\n");
 
+let listOfCar = { brand: "Toyota", model: "Avalon", licensePlate: "223200112"};
+
+
+console.log("List of car", listOfCar);
+
+let copyListOfCar1 = Object.assign({}, listOfCar);
+console.log("\nNew copy of car 1:", copyListOfCar1);
+let copyListOfCar2 = Object.assign({}, listOfCar);
+console.log("New copy of car 2:", copyListOfCar1);
+let copyListOfCar3 = Object.assign({}, listOfCar);
+console.log("New copy of car 3:", copyListOfCar1);
+let copyListOfCar4 = Object.assign({}, listOfCar);
+console.log("New copy of car 4:", copyListOfCar1);
+let copyListOfCar5 = Object.assign({}, listOfCar);
+console.log("New copy of car 5:", copyListOfCar1);
+
+
+copyListOfCar1.licensePlate = "4443323123";
+copyListOfCar2.licensePlate = "4326623234";
+copyListOfCar3.licensePlate = "5532987272";
+copyListOfCar4.licensePlate = "2748234028";
+copyListOfCar5.licensePlate = "4662322345";
+
+console.log("\nOriginal list of car: ", listOfCar);
+console.log("Change license plate for car 1: ", copyListOfCar1);
+console.log("Change license plate for car 2: ", copyListOfCar2);
+console.log("Change license plate for car 3: ", copyListOfCar3);
+console.log("Change license plate for car 4: ", copyListOfCar4);
+console.log("Change license plate for car 5: ", copyListOfCar5);
 
 /* EXERCISE 9
  Create a variable called carsForRent and assign to it an array containing all the cars from the previous exercise.
